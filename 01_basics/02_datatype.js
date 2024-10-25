c6dfdf17a919d8296181514c295c6909cffaf698
Subha Mondal <145588275+webdevbysubha@users.noreply.github.com>
2024-10-25 09:50:31 +0000

    datatype introduction

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,11 @@
+"use strict"; // recognized as newer version of js code
+
+let  number = 500
+let empName = "Subha"
+let value = true 
+let x
+let y = null
+
+console.log(typeof value)
+console.log(typeof x)
+console.log(typeof y)
