c37716f1d38a8a49f0a67577a8249d4a860c392d
Subha Mondal <145588275+webdevbysubha@users.noreply.github.com>
2024-10-25 09:50:03 +0000

    variable introduction

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const empId = 1001
+let empName = "Subha"
+var empAge = 20
+
+empSalary = 50000
+
+//empId = 1002
+
+empAge = 22
+
+/*
+console.log(empId);
+console.log(empName);
+console.log(empAge);
+*/
+
+console.log(empSalary);
+
+
+console.table([empId,empName,empAge]);
