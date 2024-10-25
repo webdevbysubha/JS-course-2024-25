fb261569670dfe6b87856890c7873d7b540a1aa0
Subha Mondal <145588275+webdevbysubha@users.noreply.github.com>
2024-10-25 11:25:39 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("1" == 1);
+console.log("1" === 1);
+
+console.log(null > 0 );
+console.log(null == 0 );
+console.log(null < 0 );
+console.log(null <= 0 );  
+
+// comparison operator and equility operator both works in different manner
+// comparison operator --> null becomes 0
+
+console.log(undefined > 0 );
+console.log(undefined == 0 );
+console.log(undefined < 0 );
+console.log(undefined <= 0 );
+
+
