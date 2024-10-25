7600a07c3e380225b82aa3ec3bdb47a61b7856c7
Subha Mondal <145588275+webdevbysubha@users.noreply.github.com>
2024-10-25 10:52:22 +0000

    conversion operation

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,49 @@
+let num = 100
+let empName = "subha"
+let value = 1
+
+//console.log(typeof(num));
+
+
+let numToString = String(num);
+//console.log(typeof(numToString));
+//console.log(numToString);
+
+let stringToNumber = Number(empName)
+//console.log(typeof(stringToNumber));
+//console.log(stringToNumber);
+
+let valueToBoolean = Boolean(value)
+
+//console.log(typeof valueToBoolean);
+//console.log(valueToBoolean);
+
+let isLoggedIn = "subha"
+let inBoolean = Boolean(isLoggedIn)
+
+// console.log(inBoolean);
+
+// **************************** OPERATION *****************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+
+console.log(1 + 2 + 3 + "4");
+console.log("1" + 2 + 3 + 4);
+
+let counter = 100
+counter++; // postfix
+++counter // prefix
+
+console.log(counter);
+
+
+
+
